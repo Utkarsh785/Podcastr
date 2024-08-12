7eeea2f561f26e607f0ba0b48a21f04c788af9a8
Utkarsh Raj <utkarshjnv002@gmail.com>
2024-08-12 09:17:14 +0530

    initial commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+ const authConfig = {
+    providers: [
+      {
+        domain: "https://pleasant-mutt-43.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ],
+  };
+  export default authConfig;
